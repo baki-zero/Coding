4e7ec76a00df4f7faa1db404d933e325952a3d8f
baki-zero <115477517+baki-zero@users.noreply.github.com>
2023-01-02 20:38:37 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner SCANNUM = new Scanner(System.in);
+		int num1, num2;
+		
+		//System.out.print("A와 B 값을 입력하세요 (0<A, B<10) : ");
+		num1 = SCANNUM.nextInt();
+		num2 = SCANNUM.nextInt();
+		
+		System.out.println(num1-num2);
+	}
+
+}
