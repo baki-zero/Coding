a6fea208c85cea56aff0fc8eca368b499b70487a
baki-zero <115477517+baki-zero@users.noreply.github.com>
2023-01-02 21:11:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		long num1, num2, num3;
+
+		num1 = scan.nextLong();
+		num2 = scan.nextLong();
+		num3 = scan.nextLong();
+
+		System.out.println(num1+num2+num3);
+		
+	}
+
+}
