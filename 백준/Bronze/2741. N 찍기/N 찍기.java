951dab9bf8f97c5b9825cad8e744e845fb1e8003
baki-zero <115477517+baki-zero@users.noreply.github.com>
2023-01-03 20:27:43 +0900

    [Bronze V] Title: N 찍기, Time: 1060 ms, Memory: 32220 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		long N;
+
+		N = scan.nextLong();
+
+		for(int i=1; i<=N; i++) {
+			System.out.println(i);
+		}
+	}
+}
