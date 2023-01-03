80961e513266691d929a6a1011e9f24e7e6f6b0a
baki-zero <115477517+baki-zero@users.noreply.github.com>
2023-01-03 23:23:56 +0900

    [Bronze V] Title: A+B - 5, Time: 236 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNextInt()) {
+		
+			int A = scan.nextInt();
+			int B = scan.nextInt();
+			
+			if((A==0)&&(B==0)) {
+				break;
+			} else {
+			System.out.println(A+B);
+			}
+		}
+	}
+}
