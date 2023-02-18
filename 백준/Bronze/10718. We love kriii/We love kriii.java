d472c16b47bdf871e7d495ee7dbb664ceabb084e
baki-zero <115477517+baki-zero@users.noreply.github.com>
2023-02-18 20:47:47 +0900

    [Bronze V] Title: We love kriii, Time: 116 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+		
+
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		
+	}
+}
