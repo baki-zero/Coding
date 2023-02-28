4afc5148a49dd251877cfe99dbd58777d09f6311
baki-zero <115477517+baki-zero@users.noreply.github.com>
2023-02-28 20:22:44 +0900

    [Bronze V] Title: 영수증, Time: 220 ms, Memory: 17844 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int sum = 0;
+		int X = sc.nextInt();
+		int N = sc.nextInt();
+		
+		for(int i=0; i<N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();		
+			sum += a*b;
+		}
+		
+		if(X == sum) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+		
+		
+	}
+}
